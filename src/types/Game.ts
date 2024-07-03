28e5272aec637bd3104fc4f30d5dd70b0ed9b8da
mario-francis <mariofrancis@yahoo.com>
2024-07-03 06:55:50 -0500

    displaying critic score

--- src/types/Game.ts
+++ src/types/Game.ts
@@ -1,8 +1,9 @@
 import Platform from "./Platform";
 
-export default interface Game{
-    id:number;
-    name:string;
-    background_image:string;
-    parent_platforms:{platform:Platform}[]
+export default interface Game {
+    id: number;
+    name: string;
+    background_image: string;
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
 }
